dfda0e2851c111d6e72823194976288269d50183
Mateo Bonino <mateo@localhost.localdomain>
2023-01-08 03:42:40 -0300

    permission

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,2 +1,5 @@
-sudo apt-get update && sudo apt-get install -y hugo make curl build-essential
-hugo new site dist
+sudo apt-get update && sudo apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
+make build
+exit 0
