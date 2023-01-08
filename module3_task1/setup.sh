b54c2381d704c696b9f4cd01c42df5fd753e259d
Mateo Bonino <mateo@localhost.localdomain>
2023-01-08 03:37:36 -0300

    permission

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install -y make wget
+sudo apt-get update && sudo apt-get install -y make wget hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 exit 0
