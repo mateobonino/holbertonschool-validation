a77dfb3101dddb1f6ba27e98d85c8ff0443685eb
Mateo Bonino <mbonino1810@gmail.com>
2023-01-12 04:11:59 -0300

    fixes

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,6 +2,7 @@ apt-get update && apt-get install -y make wget hugo npm zip nodejs
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
+npm i markdownlint
 npm install fs
 npm i -D @types/node
 exit 0
