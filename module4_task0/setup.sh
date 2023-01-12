727032d1b303f6b34190345d59cd472698a1d0ff
Mateo Bonino <mbonino1810@gmail.com>
2023-01-12 10:18:30 -0300

    fixed setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,7 @@
 if [ -d "/usr/bin/go"]
 then
-    rm -fr /usr/bin/go
+    rm -fr /usr/bin/
+fi
 apt-get update
 apt-get install -y npm zip hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
