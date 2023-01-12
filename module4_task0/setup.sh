07f443a30457542e93b1ae10648c9dd155e3908b
Mateo Bonino <mbonino1810@gmail.com>
2023-01-12 03:57:30 -0300

    added node-fs to setup.sh

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -2,5 +2,6 @@ apt-get update && apt-get install -y make wget hugo npm zip nodejs
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
 npm install -g markdownlint-cli
+npm install node-fs
 npm i -D @types/node
 exit 0
