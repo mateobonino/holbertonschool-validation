b60fdd61a261ba5b726df650a8e49adfa36ee17f
Mateo Bonino <mbonino1810@gmail.com>
2023-01-14 14:18:20 -0300

    fixed setup

--- module4_task1/setup.sh
+++ module4_task1/setup.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt-get install -y npm wget curl zip
+wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
+dpkg -i hugo_0.79.0_Linux-64bit.deb
 curl -s https://deb.nodesource.com/setup_16.x | bash
 apt-get install nodejs -y
 npm install -g markdownlint-cli
