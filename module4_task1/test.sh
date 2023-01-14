3c0b172cb64bd380bcd4851c983cbdeb35ab6bb0
Mateo Bonino <mbonino1810@gmail.com>
2023-01-14 14:09:17 -0300

    fixes

--- module4_task1/test.sh
+++ module4_task1/test.sh
@@ -1,2 +1,7 @@
-docker run -it awesome:build
-docker run awesome:build hugo
+apt-get update
+apt-get install -y npm wget curl zip
+curl -s https://deb.nodesource.com/setup_16.x | bash
+apt-get install nodejs -y
+npm install -g markdownlint-cli
+npm install markdownlint
+exit 0
