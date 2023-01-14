dfb1fc02755c68abbbc0dda4c650db5744d671c4
Mateo Bonino <mbonino1810@gmail.com>
2023-01-14 14:42:21 -0300

    fixed

--- module4_task1/test.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install -y npm wget curl zip
-curl -s https://deb.nodesource.com/setup_16.x | bash
-apt-get install nodejs -y
-npm install -g markdownlint-cli
-npm install markdownlint
-exit 0
